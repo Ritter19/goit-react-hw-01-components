61750cd6cf606d12e5dab00231313e506701e86d
Ritter19 <joey.barbacena@gmail.com>
2025-09-05 16:36:40 +0800

    Testing Final

---
+++ src/components/statistics/Statistics.jsx
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
+
+//Random Color Generator
+const getRandomHexColor = () => {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
+
+export const Statistics = ({ stats }) => {
+  return (
+    <section className={css.statistics}>
+      <h2 className={css.title}>Upload stats</h2>
+
+      <ul className={css.statList}>
+        {stats.map(stat => (
+          <li
+            className={css.item}
+            key={stat.id}
+            style={{ backgroundColor: getRandomHexColor() }}
+          >
+            <span className={css.label}>{stat.label}</span>
+            <span className={css.percentage}>{stat.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    }).isRequired
+  ),
+};
